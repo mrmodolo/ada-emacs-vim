ed220932f9a1016c0ccc8da6242a7fa4351f2929
Marcelo Módolo <marcelo.rmodolo@gmail.com>
2022-12-05 09:29:20 -0300

    Arquivos removidos

--- dir-locals.el
+++
@@ -1,4 +0,0 @@
-((nil . (
-         (eval . (setq lsp-ada-project-file "/home/modolo/playground/ada-emacs-vim/json.gpr"))
-         (eval . (setq gpr_file "/home/modolo/playground/ada-emacs-vim/json.gpr"))
-         (ada-prj-default-gpr-file . "/home/modolo/playground/ada-emacs-vim/json.gpr"))))
