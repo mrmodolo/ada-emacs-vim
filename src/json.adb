03b275d15cb639b9a519ff7c186e0cd4fc013df3
Marcelo Módolo <marcelo.rmodolo@gmail.com>
2022-12-07 13:31:55 -0300

    formatação

--- src/json.adb
+++ src/json.adb
@@ -3,15 +3,15 @@ with GNATCOLL.JSON; use GNATCOLL.JSON;
 
 procedure JSON is
 
-  --  Create a JSON value from scratch
-  My_Obj : JSON_Value := Create_Object;
+   --  Create a JSON value from scratch
+   My_Obj : JSON_Value := Create_Object;
 begin
-  My_Obj.Set_Field (Field_Name => "field1", Field => Create (Integer (1)));
-  My_Obj.Set_Field ("name", "marcelo");
-  My_Obj.Set_Field ("age", Integer (59));
-  My_Obj.Set_Field ("height", 104.0);
+   My_Obj.Set_Field (Field_Name => "field1", Field => Create (Integer (1)));
+   My_Obj.Set_Field ("name", "marcelo");
+   My_Obj.Set_Field ("age", Integer (59));
+   My_Obj.Set_Field ("height", 104.0);
 
-  --  Now serialize it. The call below will display:
-  --    {"field1": 1, "name": "thename"}
-  Put_Line (My_Obj.Write);
+   --  Now serialize it. The call below will display:
+   --    {"field1": 1, "name": "thename"}
+   Put_Line (My_Obj.Write);
 end JSON;
